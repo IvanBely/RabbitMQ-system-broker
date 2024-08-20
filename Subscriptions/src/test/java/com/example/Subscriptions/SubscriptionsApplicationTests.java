3f356bcf2fead5e463f1a39085ea827a5601d2a5
ivan <belyakov_ivan96@mail.ru>
2024-08-20 08:16:46 +0300

    init

---
+++ Subscriptions/src/test/java/com/example/Subscriptions/SubscriptionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Subscriptions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SubscriptionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
